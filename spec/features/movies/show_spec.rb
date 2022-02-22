0a81725d3a8d06b79ff85c373df5f72ad467a59c
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-22 11:49:16 -0700

    Add tests to show spec and model

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'movie show page' do
+  before(:each) do
+    @universal = Studio.create!(name: "Universal Pictures", location: "Universal City, CA")
+    @warner = Studio.create!(name: "Warner Bros", location: "Hollywood, CA")
+
+    @jurassic2 = @universal.movies.create!(title: "Jurassic World", creation_year: 2015, genre: "Action")
+    @harry = @warner.movies.create!(title: "Harry Potter and the Sorcerer's Stone", creation_year: 2001, genre: "Fantasy")
+
+    @chris = @jurassic2.actors.create!(name: "Chris Pratt", age: 45)
+    @bryce = @jurassic2.actors.create!(name: "Bryce Dallas Howard", age: 40)
+    @judy = @jurassic2.actors.create!(name: "Judy Greer", age: 50)
+
+    @emma = @harry.actors.create!(name: "Emma Watson", age: 25)
+
+    visit "/movies/#{@jurassic2.id}"
+  end
+
+  it 'shows movie attributes' do
+    expect(page).to have_content(@jurassic2.title)
+    expect(page).to have_content(@jurassic2.creation_year)
+    expect(page).to have_content(@jurassic2.genre)
+  end
+
+  it 'lists actors from youngest to oldest' do
+    expect(@bryce.name).to appear_before(@chris.name)
+    expect(@chris.name).to appear_before(@judy.name)
+  end
+
+  it 'shows avg age of actors' do
+    expect(page).to have_content("Average Actor Age: 45")
+  end
+end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,37 @@ RSpec.describe Movie do
     it { should have_many :actor_movies }
     it { should have_many(:actors).through(:actor_movies) }
   end
-end
+
+  describe 'instance methods' do
+    describe '#actors_youngest_to_oldest' do
+
+      before(:each) do
+        @universal = Studio.create!(name: "Universal Pictures", location: "Universal City, CA")
+        @jurassic2 = @universal.movies.create!(title: "Jurassic World", creation_year: 2015, genre: "Action")
+
+        @chris = @jurassic2.actors.create!(name: "Chris Pratt", age: 45)
+        @bryce = @jurassic2.actors.create!(name: "Bryce Dallas Howard", age: 40)
+        @judy = @jurassic2.actors.create!(name: "Judy Greer", age: 50)
+      end 
+
+      it 'lists actors by youngest to oldest' do
+        expect(@jurassic2.actors_youngest_to_oldest).to eq([@bryce, @chris, @judy])
+      end
+    end 
+      
+   describe '#avg_actor_age' do
+      before(:each) do
+        @universal = Studio.create!(name: "Universal Pictures", location: "Universal City, CA")
+        @jurassic2 = @universal.movies.create!(title: "Jurassic World", creation_year: 2015, genre: "Action")
+
+        @chris = @jurassic2.actors.create!(name: "Chris Pratt", age: 45)
+        @bryce = @jurassic2.actors.create!(name: "Bryce Dallas Howard", age: 40)
+        @judy = @jurassic2.actors.create!(name: "Judy Greer", age: 50)
+      end 
+      
+      it 'gives avg age of all actors in a movie' do
+        expect(@jurassic2.avg_actor_age).to eq(45)
+      end 
+    end
+  end 
+end 
