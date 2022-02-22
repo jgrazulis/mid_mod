09866951bb7682457e4b0b56aeddf77a9400c44d
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-22 12:21:33 -0700

    Add create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,15 @@
     @movie = Movie.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @movie = Movie.find(params[:id])
+    @actor = @movie.actors.create(
+      name: params[:name],
+      age: params[:age],
+    )
+    redirect_to "/movies/#{@movie.id}"
+  end 
  end
