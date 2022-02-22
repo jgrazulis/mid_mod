c8f631280e4b4a62399082ba45169f4db94562d9
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-22 11:48:29 -0700

    Add model methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :actor_movies
   has_many :actors, through: :actor_movies
+
+  def actors_youngest_to_oldest
+    actors.order(age: :asc)
+  end
+
+  def avg_actor_age
+    actors.average(:age)
+  end
 end
